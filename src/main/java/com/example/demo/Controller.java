cbd29db78f8501c790ce20e971a1e6b0630bea13
Huntehhh <casillashunter@gmail.com>
2020-12-09 12:14:51 -0500

    Commit 2

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @GetMapping("/parser")
+    @GetMapping("/2parser")
     public Parser parser(@RequestParam(value = "cf") String cf) {
-        String[] strArr = cf.split(",", 3);
-        return new Parser(strArr[0], strArr[1], strArr[2]);
+        String[] strArr = cf.split(",", 2);
+        return new Parser(strArr[0], strArr[1]);
     }
 }
--- src/main/java/com/example/demo/Parser.java
+++ src/main/java/com/example/demo/Parser.java
@@ -4,12 +4,10 @@ public class Parser {
 
     private final String cf1;
     private final String cf2;
-    private final String cf3;
 
-    public Parser(String cf1, String cf2, String cf3) {
+    public Parser(String cf1, String cf2) {
         this.cf1 = cf1;
         this.cf2 = cf2;
-        this.cf3 = cf3;
     }
 
     public String getCf1() {
@@ -20,7 +18,4 @@ public class Parser {
         return cf2;
     }
 
-    public String getCf3() {
-        return cf3;
-    }
 }
